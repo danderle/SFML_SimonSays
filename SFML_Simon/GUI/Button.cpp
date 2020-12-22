8e02831918c05b629bdc7bea1736ae59b8617623
danderle <danderle86@gmail.com>
2020-12-22 17:20:13 +0100

    In the Button class added a hovering effect

--- SFML_Simon/GUI/Button.cpp
+++ SFML_Simon/GUI/Button.cpp
@@ -16,7 +16,84 @@ void Button::SetColor(const sf::Color color)
 	shape.setFillColor(color);
 }
 
+void Button::SetMinRGB(const sf::Uint8 r, const sf::Uint8 g, const sf::Uint8 b)
+{
+	rMin = r;
+	gMin = g;
+	bMin = b;
+}
+
+void Button::SetMaxRGB(const sf::Uint8 r, const sf::Uint8 g, const sf::Uint8 b)
+{
+	rMax = r;
+	gMax = g;
+	bMax = b;
+}
+
+void Button::SetHover(const bool isHovering)
+{
+	hover = isHovering;
+}
+
+void Button::ColorTransition(const float dt)
+{
+	if (hover)
+	{
+		IncreaseRGB(dt);
+	}
+	else
+	{
+		DecreaseRGB(dt);
+	}
+}
+
+sf::FloatRect Button::GetRect() const
+{
+	return shape.getGlobalBounds();
+}
+
 void Button::Draw(sf::RenderWindow& window)
 {
 	window.draw(shape);
+}
+
+
+//Private Functions
+
+void Button::IncreaseRGB(const float dt)
+{
+	colortimePassed += dt;
+	if (colortimePassed >= colorTransitionTimeout)
+	{
+		colortimePassed = 0.f;
+		auto color = shape.getFillColor();
+		auto r = color.r == rMax ? color.r : color.r + 1;
+		auto b = color.b == bMax ? color.b : color.b + 1;
+		auto g = color.g == gMax ? color.g : color.g + 1;
+
+		color.r = r > rMax ? rMax : r;
+		color.g = g > gMax ? gMax : g;
+		color.b = b > bMax ? bMax : b;
+
+		shape.setFillColor(color);
+	}
+}
+
+void Button::DecreaseRGB(const float dt)
+{
+	colortimePassed += dt;
+	if (colortimePassed >= colorTransitionTimeout)
+	{
+		colortimePassed = 0.f; 
+		auto color = shape.getFillColor();
+		auto r = color.r == rMin ? color.r : color.r - 1;
+		auto g = color.g == gMin ? color.g : color.g - 1;
+		auto b = color.b == bMin ? color.b : color.b - 1;
+
+		color.r = r < rMin ? rMin : r;
+		color.g = g < gMin ? gMin : g;
+		color.b = b < bMin ? bMin : b;
+
+		shape.setFillColor(color);
+	}
 }
--- SFML_Simon/GUI/Button.h
+++ SFML_Simon/GUI/Button.h
@@ -11,10 +11,53 @@ public:
 	Button(const float width, const float height, const float xPos, const float yPos);
 	Button(const sf::Vector2f size, const sf::Vector2f position);
 
+	/// <summary>
+	/// Set the default background color
+	/// </summary>
+	/// <param name="color"></param>
 	void SetColor(const sf::Color color);
+
+	/// <summary>
+	/// Set the min rgb values when using the hover effect
+	/// </summary>
+	/// <param name="r"></param>
+	/// <param name="g"></param>
+	/// <param name="b"></param>
+	void SetMinRGB(const sf::Uint8 r, const sf::Uint8 g, const sf::Uint8 b);
+
+	/// <summary>
+	/// Set the max values for using the hover effect
+	/// </summary>
+	/// <param name="r"></param>
+	/// <param name="g"></param>
+	/// <param name="b"></param>
+	void SetMaxRGB(const sf::Uint8 r, const sf::Uint8 g, const sf::Uint8 b);
+
+	void SetHover(const bool isHovering);
+
+	void ColorTransition(const float dt);
+
+	sf::FloatRect GetRect() const;
+
 	void Draw(sf::RenderWindow& window);
+
+private:
+	void IncreaseRGB(const float dt);
+	void DecreaseRGB(const float dt);
+
 private:
 	sf::RectangleShape shape;
+	sf::Uint8 rMin;
+	sf::Uint8 gMin;
+	sf::Uint8 bMin;
+	sf::Uint8 rMax;
+	sf::Uint8 gMax;
+	sf::Uint8 bMax;
 
+	bool hover = false;
+	float colortimePassed = 0;
+
+private:
+	static constexpr float colorTransitionTimeout = 0.1f;
 };
 
