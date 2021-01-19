9b8efdca125fd09b0b666914ce4a71429a7576ae
danderle <danderle86@gmail.com>
2021-01-19 07:26:24 +0100

    Added the StateMachine to handle accessing and updating all the states for this game

---
+++ SFML_Simon/GameStateMachine/StateMachine.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <memory>
+#include <stack>
+#include "IState.h"
+
+class StateMachine
+{
+public:
+	StateMachine() = default;
+
+	void AddState(std::unique_ptr<IState> _newState, bool _isReplacing = true);
+	void RemoveState();
+	void ProcessStateChanges();
+
+	std::unique_ptr<IState>& GetActiveState();
+
+private:
+	std::stack<std::unique_ptr<IState>> states;
+	std::unique_ptr<IState> newState;
+	bool isRemoving = false;
+	bool isAdding = false;
+	bool isReplacing = false;
+};
