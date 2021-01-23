86c1d7a59a4bcd3230dd79a2fbc135cb7d14a46d
danderle <danderle86@gmail.com>
2021-01-23 07:33:12 +0100

    Added blinking and toggeling text to the FieldCenter class. Changed the Enum Turn to State and added GameOver and PressToContinue

--- SFML_Simon/FieldCenter.cpp
+++ SFML_Simon/FieldCenter.cpp
@@ -21,37 +21,98 @@ void FieldCenter::Draw(sf::RenderWindow& window)
 	secondLine.Draw(window);
 }
 
-void FieldCenter::SetTurn(const Turn turn)
+void FieldCenter::SetState(const State state)
 {
-	switch (turn)
+	currentState = state;
+	switch (currentState)
 	{
-	case Turn::PressToStart:
+	case State::PressToStart:
 		firstLine.SetString("Press red");
 		secondLine.SetString("to start");
 		SetTextColor(pressStartColor);
+		StopBlink();
 		break;
-	case Turn::SimonSays:
+	case State::SimonSays:
 		firstLine.SetString("Simon");
 		secondLine.SetString("says");
 		SetTextColor(simonsSaysColor);
 		break;
-	case Turn::PleaseRepeat:
+	case State::PleaseRepeat:
 		firstLine.SetString("Please");
 		secondLine.SetString("repeat");
 		SetTextColor(pleaseRepeatColor);
 		break;
-	case Turn::GameOver:
+	case State::GameOver:
 		firstLine.SetString("Game");
 		secondLine.SetString("over");
 		SetTextColor(gameOverColor);
+		Blink();
+		break;
+	case State::PressToContinue:
+		firstLine.SetString("Press to");
+		secondLine.SetString("continue");
+		SetTextColor(gameOverColor);
+		Blink();
 		break;
 	}
 	CenterText();
 }
 
+const State FieldCenter::GetState() const
+{
+	return currentState;
+}
+
+void FieldCenter::ColorTransition(const float dt)
+{
+	if (toggleText)
+	{
+		toggletimePassed += dt;
+		if (toggletimePassed >= toggleTransitionTimeout)
+		{
+			ToggleText();
+			toggletimePassed = 0;
+		}
+	}
+	if (isBlinking && increaseRgb)
+	{
+		IncreaseRGB(dt);
+	}
+	else if (isBlinking && decreaseRgb)
+	{
+		DecreaseRGB(dt);
+	}
+}
+
 
 //Private Methods
 
+void FieldCenter::ToggleText()
+{
+	if (currentState == State::GameOver)
+	{
+		SetState(State::PressToContinue);
+	}
+	else
+	{
+		SetState(State::GameOver);
+	}
+}
+
+void FieldCenter::Blink()
+{
+	toggleText = true;
+	isBlinking = true;
+	increaseRgb = true;
+}
+
+void FieldCenter::StopBlink()
+{
+	toggleText = false;
+	isBlinking = false;
+	increaseRgb = false;
+	decreaseRgb = false;
+}
 
 void FieldCenter::CenterText()
 {
@@ -80,6 +141,9 @@ void FieldCenter::SetupCircle(const sf::Vector2f windowSize)
 	circle.setOrigin(circle.getRadius(), circle.getRadius());
 	circle.setPosition(centerPoint);
 	circle.setFillColor({ 192,192,192 });
+	rMin = gMin = bMin = 192;
+	rMax = gMax = bMax = 255;
+
 	circle.setOutlineThickness(circleOutlineThickness);
 	circle.setOutlineColor(outlineColor);
 }
@@ -94,5 +158,64 @@ void FieldCenter::SetupTexts(const sf::Vector2f windowSize)
 	secondLine.SetPosition(windowSize.x / 2, windowSize.y / 2 + secondLine.GetHeight() * 3/4);
 	SetTextColor(pressStartColor);
 	SetTextOutline(outlineColor, textOutlineThickness);
+}
+
+void FieldCenter::DecreaseRGB(const float dt)
+{
+	assert(rMax != -1);
+	assert(gMax != -1);
+	assert(bMax != -1);
+	colortimePassed += dt;
+	if (colortimePassed >= colorTransitionTimeout)
+	{
+		colortimePassed = 0.f;
+		auto color = circle.getFillColor();
+		auto r = color.r == rMin ? color.r : color.r - colorSteps;
+		auto g = color.g == gMin ? color.g : color.g - colorSteps;
+		auto b = color.b == bMin ? color.b : color.b - colorSteps;
+
+		color.r = r < rMin ? rMin : r;
+		color.g = g < gMin ? gMin : g;
+		color.b = b < bMin ? bMin : b;
+
+		if (color.r == rMin &&
+			color.g == gMin &&
+			color.b == bMin)
+		{
+			decreaseRgb = false;
+			increaseRgb = true;
+		}
+		circle.setFillColor(color);
+	}
+}
+
+void FieldCenter::IncreaseRGB(const float dt)
+{
+	assert(rMax != -1);
+	assert(gMax != -1);
+	assert(bMax != -1);
+	colortimePassed += dt;
+	if (colortimePassed >= colorTransitionTimeout)
+	{
+		colortimePassed = 0.f;
+		auto color = circle.getFillColor();
+		auto r = color.r == rMax ? color.r : color.r + colorSteps;
+		auto b = color.b == bMax ? color.b : color.b + colorSteps;
+		auto g = color.g == gMax ? color.g : color.g + colorSteps;
+
+		color.r = r > rMax ? rMax : r;
+		color.g = g > gMax ? gMax : g;
+		color.b = b > bMax ? bMax : b;
+
+		if (color.r == rMax &&
+			color.g == gMax &&
+			color.b == bMax)
+		{
+			increaseRgb = false;
+			decreaseRgb = true;
+		}
+
+		circle.setFillColor(color);
+	}
 }
 
--- SFML_Simon/FieldCenter.h
+++ SFML_Simon/FieldCenter.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "SFML/Graphics.hpp"
 #include "Text.h"
+#include <assert.h>
 
-enum class Turn
+enum class State
 {
 	PressToStart,
 	SimonSays,
 	PleaseRepeat,
-	GameOver
+	GameOver,
+	PressToContinue
 };
 
 class FieldCenter
@@ -17,23 +19,49 @@ public:
 
 	bool Contains(const sf::Vector2f mousePosition) const;
 	void Draw(sf::RenderWindow& window);
-	void SetTurn(const Turn turn);
+	void SetState(const State State);
+	const State GetState() const;
+	void ColorTransition(const float dt);
 
 private:
+	void ToggleText();
+	void Blink();
+	void StopBlink();
 	void CenterText();
 	void SetTextColor(const sf::Color color);
 	void SetTextOutline(const sf::Color color, const float thickness);
 	void SetupCircle(const sf::Vector2f windowSize);
 	void SetupTexts(const sf::Vector2f windowSize);
+	void DecreaseRGB(const float dt);
+	void IncreaseRGB(const float dt);
+
 private:
 	sf::CircleShape circle;
+	sf::Vector2f centerPoint;
+	sf::Color defaultColor;
+	sf::Uint8 rMin;
+	sf::Uint8 gMin;
+	sf::Uint8 bMin;
+	sf::Uint8 rMax;
+	sf::Uint8 gMax;
+	sf::Uint8 bMax;
+
 	Text firstLine;
 	Text secondLine;
-	sf::Vector2f centerPoint;
+	State currentState;
+	bool isBlinking = false;
+	bool decreaseRgb = false;
+	bool increaseRgb = false;
+	bool toggleText = false;
+	float colortimePassed = 0;
+	float toggletimePassed = 0;
 
+	static constexpr float toggleTransitionTimeout = 2.f;
+	static constexpr float colorTransitionTimeout = 0.1f;
 	static constexpr float circleRadius = 98.f;
 	static constexpr float circleOutlineThickness = -8.f;
 	static constexpr float textOutlineThickness = 1.f;
+	static constexpr sf::Uint8 colorSteps = 25;
 	const sf::Color outlineColor = sf::Color::Black;
 	const sf::Color pressStartColor = sf::Color::Black;
 	const sf::Color simonsSaysColor = sf::Color(255, 0, 255);
